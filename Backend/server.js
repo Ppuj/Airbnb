4580f48b5f34b06e8202a53775d3e8e992d5e1c3
pooja pandit <102018934+Ppuj@users.noreply.github.com>
2026-01-14 18:31:09 +0530

    commit2

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-debugger;
 const express = require("express");
 const cors = require("cors");
 // require("dotenv").config();
@@ -20,7 +19,7 @@ app.use("/api/bookings", bookingRoutes);
 connectDB(); 
 
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
