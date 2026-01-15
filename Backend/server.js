f975a7787d17b7e9ad14cd5542ee99fa3260600f
pooja pandit <102018934+Ppuj@users.noreply.github.com>
2026-01-15 15:18:35 +0530

    commit8

--- Backend/server.js
+++ Backend/server.js
@@ -17,9 +17,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use(  express.static(path.join(__dirname, "..", "Frontend", "dist")))
-app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,"..","Frontend","dist","index.html"));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "Frontend", "dist", "index.html"));
+});
 connectDB(); 
 
 
